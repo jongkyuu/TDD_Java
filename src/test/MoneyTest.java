0554d89ba6f2f91ae9bc079c1823eca00ce9bf70
minerva421 <minerva421.ai@gmail.com>
2022-10-16 03:41:33 +0900

    TDD - 1장

---
+++ src/test/MoneyTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+	@Test
+	void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+}
+
+class Dollar{
+	int amount;
+
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	void times(int multiplier) {
+		amount *= multiplier;
+	}
+}
