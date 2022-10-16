e2072215ee89be5fd574e3fbb984a676498a66a2
minerva421 <minerva421.ai@gmail.com>
2022-10-17 01:24:19 +0900

    TDD - 2장

--- src/test/MoneyTest.java
+++ src/test/MoneyTest.java
@@ -8,19 +8,21 @@ class MoneyTest {
 	@Test
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 }
 
 class Dollar{
 	int amount;
 
-	public Dollar(int amount) {
+	Dollar(int amount) {
 		this.amount = amount;
 	}
 	
-	void times(int multiplier) {
-		amount *= multiplier;
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
 }
